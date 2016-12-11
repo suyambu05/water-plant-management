bb1016493d489f708dbbb608d578c6c13a1abedc
Home <Home@Home-PC>
2016-12-11 13:43:08 +0530

    WPM web services base...

---
+++ wpm-services/wpm-web/src/main/java/org/v3/web/WPMApplication.java
@@ -0,0 +1,21 @@
+package org.v3.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Dineshkumar
+ *
+ */
+@SpringBootApplication
+public class WPMApplication {
+
+	/**
+	 * Method to launch the web application 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(WPMApplication.class, args);
+	}
+
+}
---
+++ wpm-services/wpm-webobjects/src/main/java/org/v3/webobjects/LoginResponse.java
@@ -0,0 +1,25 @@
+package org.v3.webobjects;
+
+/**
+ * @author Dineshkumar
+ *
+ */
+public class LoginResponse {
+	
+	private boolean returnValue;
+
+	/**
+	 * @return the returnValue
+	 */
+	public boolean isReturnValue() {
+		return returnValue;
+	}
+
+	/**
+	 * @param returnValue the returnValue to set
+	 */
+	public void setReturnValue(boolean returnValue) {
+		this.returnValue = returnValue;
+	}
+
+}
